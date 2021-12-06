0f153a9f5c6b51b3b840d3c9b03c0b1dbbb3fefb
Casper Verswijvelt <casperverswijvelt@gmail.com>
2021-12-06 20:16:11 +0100

    Day 1 part 2

--- 1/index.js
+++ 1/index.js
@@ -5,14 +5,33 @@ const lines = text.split("\r\n");
 
 const numbers = lines.map((line) => parseInt(line));
 
+let previousSum
+let previousPreviousNumber
 let previousNumber 
 let increasedCount = 0
+let sumIncreasedCount = 0
 
 numbers.forEach((num) => {
+
   if (previousNumber && num > previousNumber) {
       increasedCount++
   }
+
+  if (previousNumber && previousPreviousNumber) {
+
+    const sum = previousPreviousNumber + previousNumber + num
+
+    if (previousSum && sum > previousSum) {
+
+        sumIncreasedCount++
+    }
+
+    previousSum = sum
+  } 
+
+  previousPreviousNumber = previousNumber
   previousNumber = num
 });
 
 console.log(`Increase count: ${increasedCount}`)
+console.log(`Sum increase count: ${sumIncreasedCount}`)
