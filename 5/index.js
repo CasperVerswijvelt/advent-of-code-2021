56ca5e2908bddffd11764da45cdc7b79f66208f1
Casper Verswijvelt <casperverswijvelt@gmail.com>
2021-12-07 18:01:41 +0100

    Day 5 part 2

--- 5/index.js
+++ 5/index.js
@@ -17,12 +17,15 @@ let count = 0
 
 lines.forEach((line) => {
 
+  let minX = Math.min(line.x1, line.x2)
+  let maxX = Math.max(line.x1, line.x2)
+
+  let maxY = Math.max(line.y1, line.y2)
+  let minY = Math.min(line.y1, line.y2)
+
   if (line.x1 === line.x2) {
     
-    let max = Math.max(line.y1, line.y2)
-    let min = Math.min(line.y1, line.y2)
-
-    for (let i = min; i <= max; i ++) {
+    for (let i = minY; i <= maxY; i++) {
 
       incrementPos(line.x1, i)
     }
@@ -30,20 +33,38 @@ lines.forEach((line) => {
 
   if (line.y1 === line.y2) {
 
-    let max = Math.max(line.x1, line.x2)
-    let min = Math.min(line.x1, line.x2)
-
-    for (let i = min; i <= max; i ++) {
+    for (let i = minX; i <= maxX; i++) {
 
       incrementPos(i, line.y1)
     }
   }
+
+  if (Math.abs(line.y1 - line.y2) === Math.abs(line.x1 - line.x2)) {
+
+    let startY
+    
+    if (minX === line.x1) {
+
+      startY = line.y1
+    } else {
+
+      startY = line.y2
+    }
+
+    let incr = startY === minY
+
+    for (let x = minX; x <= maxX; x ++) {
+
+      incrementPos(x, startY + ((x-minX)*(incr? 1 : -1)))
+    }
+  }
 });
+//console.log(grid.map(arr => arr.join('')).join('\n'))
 
 function incrementPos(x,y) {
 
   if (!grid[y]) {
-    grid[y] = []
+    grid[y] = [];
   }
 
   grid[y][x] = grid[y][x] ? grid[y][x] + 1 : 1
