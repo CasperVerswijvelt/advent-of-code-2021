842a379df6dcbd624190a85934a53ea151733933
Casper Verswijvelt <casperverswijvelt@gmail.com>
2021-12-09 16:22:57 +0100

    Day 9 part 2

--- 9/index.js
+++ 9/index.js
@@ -5,14 +5,50 @@ const text = fs.readFileSync(process.argv[2]).toString();
 const grid = text.split("\r\n").map((line) => line.split("").map(text => parseInt(text)));
 
 let sum = 0
+let basinSizes = []
+const lowPoints = []
+const checkedCoordinates = grid.map(row => row.slice().fill(false))
 
 for (let row = 0; row < grid.length; row++) {
   for (let col = 0; col < grid[row].length; col++) {
     if (isLowPoint(row, col)) {
+      lowPoints.push({
+        row: row,
+        col: col
+      })
       let risk = 1 + grid[row][col]
       sum += risk
     }
   }
+}
+
+lowPoints.forEach(lowPoint => {
+  let basinSize = findBasinSize(lowPoint.row, lowPoint.col)
+  if (basinSize) {
+    basinSizes.push(basinSize)
+  }
+})
+
+function findBasinSize(row, col) {
+
+  if (
+    grid[row] &&
+    typeof grid[row][col] === 'number' &&
+    !checkedCoordinates[row][col]
+  ) {
+
+    checkedCoordinates[row][col] = true
+
+    if (grid[row][col] === 9) return 0
+
+    return 1 + 
+      findBasinSize(row-1,col) +
+      findBasinSize(row+1,col) +
+      findBasinSize(row,col-1) +
+      findBasinSize(row,col+1)
+  }
+
+  return 0
 }
 
 function isLowPoint(row, col) {
@@ -37,3 +73,4 @@ function isLowPoint(row, col) {
 }
 
 console.log(`Sum of low point risks: ${sum}`)
+console.log(`Product of basin sizes: ${basinSizes.sort((a,b) => b-a).slice(0,3).reduce((a,b)=>a*b)}`)
