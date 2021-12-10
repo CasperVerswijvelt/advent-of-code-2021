13e5d68f78d37b71b1cb002e96f8448108013ee5
Casper Verswijvelt <casperverswijvelt@gmail.com>
2021-12-10 18:03:20 +0100

    Day 10 part 2

--- 10/index.js
+++ 10/index.js
@@ -4,6 +4,7 @@ const text = fs.readFileSync(process.argv[2]).toString();
 const lines = text.split("\r\n");
 
 let totalScore = 0
+const completionScores = []
 
 lines.forEach((line) => {
 
@@ -11,7 +12,7 @@ lines.forEach((line) => {
 
   const stack = []
 
-  let score
+  let incomplete = true
 
   for (let character of chars) {
 
@@ -23,16 +24,32 @@ lines.forEach((line) => {
       if (stack[stack.length-1] === character) {
         stack.pop()
       } else {
+        incomplete = false
         let score = getCharacterScore(character)
         totalScore += score
-        console.log(`Expected ${stack[stack.length-1]}, but found ${character} instead (score ${score})`)
+        console.log(`Syntax error: expected ${stack[stack.length-1]}, but found ${character} instead (score ${score})`)
         break
       }
     }
   }
+
+  if (incomplete) {
+
+    let score = 0
+
+    for (let i = stack.length - 1; i >= 0; i--) {
+      score *= 5
+      score += getCompletionScore(stack[i])
+    }
+
+    completionScores.push(score)
+
+    console.log(`Incomplete string: adding '${stack.reverse().join("")}' (score ${score})`)
+  }
 });
 
-console.log(`Total score: ${totalScore}`)
+console.log(`\nTotal score: ${totalScore}`)
+console.log(`Middle completion score: ${completionScores.sort((a,b)=>a-b)[(completionScores.length-1)/2]}`)
 
 function getClosingChar(character) {
   switch(character) {
@@ -59,6 +76,21 @@ function getCharacterScore(character) {
       return 57
     case ')':
       return 3
+    default:
+      return 0
+  }
+}
+
+function getCompletionScore(character) {
+  switch(character) {
+    case '}':
+      return 3
+    case '>':
+      return 4
+    case ']':
+      return 2
+    case ')':
+      return 1
     default:
       return 0
   }
